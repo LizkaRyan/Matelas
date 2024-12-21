36eb771ce9da3f12216ee65bfec37d9d44b59616
Ryan <lizkaryan626@gmail.com>
2024-12-21 13:50:27 +0300

    Update TransformationConfig.java

--- src/main/java/mg/itu/matelas/config/TransformationConfig.java
+++ src/main/java/mg/itu/matelas/config/TransformationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class TransformationConfig {
+    //Test
     @Value("${transformation.margin.percentage}")
     private double percentage;
 }
